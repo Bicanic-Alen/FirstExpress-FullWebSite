9c9eb7f6a491746219a45b7029577c9fdf871875
Bicanic-Alen <bicanic.alen@iisgalvanimi.edu.it>
2020-11-09 11:56:40 +0000

    v2.0.1

--- EXPRESSAPP_WEBSITE/index.js
+++ EXPRESSAPP_WEBSITE/index.js
@@ -1,6 +1,14 @@
-var express = require('express');
-var app = express(); 
+// Get dependencies
+const express = require('express');
 const path = require('path');
+const http = require('http');
+const app= express();
+
+var cors = require('cors'); //HTTP access control (CORS) for cross origin requests
+
+app.use(cors()); //Setup cors
+
+app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('/',function(req,res){
  res.sendFile(path.join(__dirname, 'index.html')); //__dirname : Ritorna la cartella del progetto
@@ -12,6 +20,10 @@ app.get('/about',function(req,res){
 
 app.get('/sitemap',function(req,res){
  res.sendFile(path.join(__dirname,'sitemap.html')); //__dirname : Ritorna la cartella del progetto
+});
+
+app.get('/api/products',function(req,res){
+ res.sendFile(path.join(__dirname,'products.json')); //__dirname : Ritorna la cartella del progetto
 });
 
 app.listen(3000, function () {
