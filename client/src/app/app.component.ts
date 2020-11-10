3eacb5cbd8d1cb8d07bc9ae87840a0b3dda63f14
Bicanic-Alen <bicanic.alen@iisgalvanimi.edu.it>
2020-11-10 09:31:22 +0000

    v2.1F

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'client';
+    results: string[];
+    obs:Observable<object>;
+    // Inject HttpClient into your component or service.
+    constructor(private http: HttpClient) {}
+
+    ngOnInit(): void {
+      // Make the HTTP request:
+    }
+
+    event():void{
+      this.obs = this.http.get('https://3000-f58b7863-f975-4504-ad64-122cf954247d.ws-eu01.gitpod.io/api/products');
+      this.obs.subscribe(this.getData);
+    }
+    getData = data => {
+        this.results = data['products'];
+      }
 }
+
